920e8bf92afc7415b002badbbe59322e28ad3b03
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-07-01 17:54:04 +0430

    this simple game with js completed

---
+++ main.js
@@ -0,0 +1,35 @@
+const choices=["rock","paper","scissors"];
+
+const userChoice=prompt("Choose rock , paper , scissors");
+
+if(userChoice){
+    console.log(`You choose: ${userChoice}`)
+}else{
+    console.log("You cheated!")
+}
+
+const randomeNumber=Math.floor(Math.random()*3);
+const computerChoice=choices[randomeNumber];
+console.log(`Computer chooses: ${computerChoice}`);
+
+if(userChoice===computerChoice){
+    console.log("It's a tie!")
+}else if(userChoice==="rock"){
+    if(computerChoice==="scissors"){
+        console.log("You win!")
+    }else{
+        console.log("You lose!")
+    }
+}else if(userChoice==="paper"){
+    if(computerChoice==="rock"){
+        console.log("You win!") 
+    }else{
+        console.log("You lose!")
+    }
+}else if(userChoice==="scissors"){
+    if(computerChoice==="paper"){
+        console.log("You win!") 
+    }else{
+        console.log("You lose!")
+    }
+}
